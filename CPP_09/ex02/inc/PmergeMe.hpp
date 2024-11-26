b42502a03cf9e556721c907d91e785e881a0856b
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2024-11-26 18:33:20 +0100

    hpp order

--- CPP_09/ex02/inc/PmergeMe.hpp
+++ CPP_09/ex02/inc/PmergeMe.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaronespinosa <aaronespinosa@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:07:46 by aaespino          #+#    #+#             */
-/*   Updated: 2024/11/23 18:41:01 by aaespino         ###   ########.fr       */
+/*   Updated: 2024/11/26 18:30:44 by aaronespino      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>  // Necesario para std::find
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <deque>
@@ -31,20 +31,10 @@
 #define CYAN "\033[36m"
 #define RESET "\033[0m"
 #define BOLD "\033[1m"
-#define HIGHLIGHT "\033[1;41m" // Fondo rojo con texto en negrita
-
-struct Stack_node {
-    int left;
-    int right;
-};
-
-struct Stack {
-    std::vector<Stack_node> left;
-    std::vector<Stack_node> right;
-};
+#define HIGHLIGHT "\033[1;41m"
 
 //                      MANIPULACION DE STACK
-// Funciones relacionadas con la comparación
+// Funciones relacionadas con la comprobación
 int compArgs(char **argv, int argc);
 int compRep(int target, char** set, int limit);
 
@@ -55,33 +45,23 @@ std::deque<int> getDeque(char **nums, int argc);
 std::deque<int> getStack(std::deque<int>& stack, int end, int group_size, int side);
 // Funciones de impresión
 void printDeque(const std::deque<int>& stack, int size, const std::string& name);
-void printToSort(const Stack_node* to_sort, int size);
 
-//                      ALGO
-// binary_search.cpp
-int littleBinarySearch(const std::deque<int>& chain, int end, int value);
+//                      ALGO DEQUE
 int binarySearch(const std::deque<int>& chain, int end, int value, int size);
-// jacobsthal.cpp
 void insertWithJacobsthal(std::deque<int>& stack, int end, int group_size);
-// msi.cpp
 void msi(std::deque<int>& stack, int end, int size);
 
-//                      DEQUE
+//                      VECTOR
 
 // Funciones relacionadas con la manipulación de cadenas
 std::vector<int> getVector(char **nums, int argc);
 std::vector<int> getStack(std::vector<int>& stack, int end, int group_size, int side);
 // Funciones de impresión
 void printVector(const std::vector<int>& stack, int size, const std::string& name);
-void printToSort(const Stack_node* to_sort, int size);
 
-//                      ALGO
-// binary_search.cpp
-int littleBinarySearch(const std::vector<int>& chain, int end, int value);
+//                      ALGO VECTOR
 int binarySearch(const std::vector<int>& chain, int end, int value, int size);
-// jacobsthal.cpp
 void insertWithJacobsthal(std::vector<int>& stack, int end, int group_size);
-// msi.cpp
 void msi(std::vector<int>& stack, int end, int size);
 
 #endif
