5e2ad3dd1551d040ded0722349e9de4ce35d46ea
Aaron Espinosa <95354392+spnzed@users.noreply.github.com>
2024-11-27 23:22:09 +0100

    rm the const one

--- CPP_08/ex00/inc/easyfind.hpp
+++ CPP_08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: aaronespinosa <aaronespinosa@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 12:25:41 by aaespino          #+#    #+#             */
-/*   Updated: 2024/11/27 23:08:56 by aaronespino      ###   ########.fr       */
+/*   Updated: 2024/11/27 23:21:34 by aaronespino      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,6 @@ int easyfind(T container, int n) {
     }
 
 	return *it;
-}
-
-template <typename T>
-int easyfind(const T& container, int n) {
-    typename T::const_iterator it = std::find(container.begin(), container.end(), n);
-
-    if (it == container.end()) {
-        throw std::out_of_range("Value not found in container");
-    }
-
-    return *it;
 }
 
 #endif
